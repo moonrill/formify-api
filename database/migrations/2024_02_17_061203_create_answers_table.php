a75738a4c0e7a9063020168f2ca5c1fb010f82f6
moonrill <arilramadani245@gmail.com>
2024-02-17 13:24:13 +0700

    setup database & migration

---
+++ database/migrations/2024_02_17_061203_create_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('response_id');
+            $table->unsignedBigInteger('question_id');
+            $table->text('value');
+
+            $table->foreign('response_id')->references('id')->on('responses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
