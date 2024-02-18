a234fce315aaabd19669911ea4dd541da377d9ef
moonrill <arilramadani245@gmail.com>
2024-02-18 10:52:57 +0700

    feat : default user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name'     => 'user1',
+            'email'    => 'user1@webtech.id',
+            'password' => Hash::make('password1'),
+        ]);
+        \App\Models\User::factory()->create([
+            'name'     => 'user2',
+            'email'    => 'user2@webtech.id',
+            'password' => Hash::make('password2'),
+        ]);
+        \App\Models\User::factory()->create([
+            'name'     => 'user3',
+            'email'    => 'user3@webtech.id',
+            'password' => Hash::make('password3'),
+        ]);
     }
 }
